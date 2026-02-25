2aca62bf07ecbf75ccdfdaca6d0277db1da6c9f8
Dan Handford <danhandford1994@gmail.com>
2026-02-25 11:34:24 +0000

    fix recording callback

--- app/api/twilio/route.ts
+++ app/api/twilio/route.ts
@@ -2,19 +2,17 @@ import { NextResponse } from "next/server"
 import { twiml } from "twilio"
 
 export async function POST(req: Request) {
-  const formData = await req.formData()
-
-  const callSid = formData.get("CallSid") as string
-  const from = formData.get("From") as string
-
   const response = new twiml.VoiceResponse()
 
   response.say("Please leave a message after the beep.")
 
   response.record({
     maxLength: 30,
+    playBeep: true,
+    trim: "trim-silence",
     recordingStatusCallback: "https://www.plumbercallguard.co.uk/api/twilio/recording",
-    recordingStatusCallbackMethod: "POST"
+    recordingStatusCallbackMethod: "POST",
+    recordingStatusCallbackEvent: ["completed"]
   })
 
   return new NextResponse(response.toString(), {
